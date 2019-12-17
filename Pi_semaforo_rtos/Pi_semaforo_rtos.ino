c8a1e5ad21ace15996c3efb5399d42f553dca89f
Gervasio Gesse <gervasio.81@gmail.com>
2019-12-17 02:28:51 -0300

    Veersao funcional

--- Pi_semaforo_rtos/Pi_semaforo_rtos.ino
+++ Pi_semaforo_rtos/Pi_semaforo_rtos.ino
@@ -8,6 +8,9 @@
  * visuais e pessoas com mobilidade reduzida.
  */
  #include <Arduino_FreeRTOS.h>
+ #include <semphr.h>
+ #include "string.h"
+ #include <SoftwareSerial.h>
 
 //Define a saida para os leds
 const int vermelho = 4;
@@ -18,16 +21,52 @@ const int p_verde = 8;
 const int botao = 2;
 
 
+//Define os pinos para a serial   
+SoftwareSerial mySerial(10, 11); // RX, TX 
+
+// Define a estrutura do semaforo
+SemaphoreHandle_t xSerialSemaphore;
+
+// Define buffer
+char bufferPC[64] = {'\0'};
+char bufferBT[64] = {'\0'};
+
+
+// Define o tempo em segundos para os estados do semaforo
+int t_vermelho = 1, t_amarelo = 1, t_verde = 1, t_adicional;
+
 // Define as tasks
 void TaskBlink( void *pvParameters );
-void TaskAnalogRead( void *pvParameters );
+void TaskSemaforoPrincipal( void *pvParameters );
+void TaskComSerial( void *pvParameters );
+void TaskBluetoothSerial( void *pvParameters );
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  mySerial.begin(9600);
   
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+
+    if ( xSerialSemaphore == NULL )  // Verifica se o semaforo existe e se não existir cria
+  {
+    xSerialSemaphore = xSemaphoreCreateMutex();  // Cria um semaforo mutex
+    if ( ( xSerialSemaphore ) != NULL )
+      xSemaphoreGive( ( xSerialSemaphore ) );  // Libera o semaforo
+  }
+
+    if ( xSerialSemaphore != NULL )
+  {
+    //Cria as tasks de comunicacao
+    xTaskCreate(TaskComSerial, "ComSerial", 128, NULL, 2, NULL );
+    xTaskCreate(TaskBluetoothSerial, "BluetoothSerial", 128, NULL, 2, NULL );
+        
+  }
+  else
+  {
+    Serial.println("**** Erro ao criar semaforo!");
   }
 
   // Now set up two tasks to run independently.
@@ -40,19 +79,11 @@ void setup() {
     ,  NULL );
 
   xTaskCreate(
-    TaskDigitalRead
-    ,  (const portCHAR *) "BotaoRead"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
-
-  xTaskCreate(
     TaskSemaforoPrincipal
     ,  (const portCHAR *) "SemaforoPrincipal"
     ,  128  // Stack size
     ,  NULL
-    ,  1  // Priority
+    ,  2  // Priority
     ,  NULL );
 
 }
@@ -70,38 +101,99 @@ void TaskBlink(void *pvParameters)  // Task blink para teste.
   (void) pvParameters;
   // inicializa digital LED_BUILTIN interno no pino 13 como output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(botao, INPUT_PULLUP);
 
   for (;;) // A Task shall never return or exit.
   {
+    if(digitalRead(botao) == 0){
+      t_vermelho = 10;
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else{
     digitalWrite(LED_BUILTIN, HIGH);   // liga o led interno
     vTaskDelay( 200 / portTICK_PERIOD_MS ); // aguarda um segundo
     digitalWrite(LED_BUILTIN, LOW);    // desliga o led interno
     vTaskDelay( 4800 / portTICK_PERIOD_MS ); // aguarda um segundo
+    }
   }
 }
 
-void TaskDigitalRead(void *pvParameters)  //Task para ler o botao.
+void TaskComSerial(void *pvParameters)
 {
   (void) pvParameters;
-  int val, oldVal=1;
-  unsigned long startTime = 0;
-  int brilho = 0;
-  //define o pino verde como saida
-  pinMode(verde, OUTPUT); 
-  // define o pino botão como entrada e com pullup interno ativo
-  pinMode ( botao, INPUT_PULLUP);
-
+  char caractere;
+  int i=0;
   for (;;)
   {
-    startTime = millis();
-    val = digitalRead(botao);
-    //while(val == 0 && (millis() - startTime) > 500) && oldVal == 1 {
-    while(val == 0){
-      Serial.println(millis() - startTime);
-      vTaskDelay(pdMS_TO_TICKS(1000));
-      val = digitalRead(botao);
+    //Trocar portMAX_DELAY por ( TickType_t ) 5 para ticks e deposi desistir
+    if ( xSemaphoreTake( xSerialSemaphore,  ( TickType_t ) 5 ) == pdTRUE )
+    {
+  
+      // Enquanto receber algo pela serial
+      while(Serial.available() > 0) {
+        // Lê byte da serial PC
+        caractere = Serial.read();
+        Serial.print("Recebi: ");
+        Serial.println(caractere);
+        // Concatena valores
+        bufferPC[i] = caractere;
+        i++;
+        bufferPC[i]= '\0';
+        // Aguarda buffer serial ler próximo caractere
+        vTaskDelay(1);
       }
-    vTaskDelay(1);  // 1 tick delay (15ms)
+      i=0;
+      if(bufferBT[0] != '\0'){
+        Serial.println(bufferBT);
+        if(strncmp(bufferBT, "+DISC:SUCCESS", 13) == 0){
+         Serial.println("DesConectado");
+        }
+        if(strncmp(bufferBT, "OK", 2) == 0){
+         Serial.println("Conectado");
+         t_vermelho = 10;
+        }
+      }
+      bufferBT[0] = '\0'; 
+      xSemaphoreGive( xSerialSemaphore );
+
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+void TaskBluetoothSerial(void *pvParameters)
+{
+  (void) pvParameters;
+  char caractere;
+  int i=0;
+  for (;;)
+  {
+    //Trocar portMAX_DELAY por ( TickType_t ) 5 para ticks e deposi desistir
+    if ( xSemaphoreTake( xSerialSemaphore,  ( TickType_t ) 5 ) == pdTRUE )
+    {
+  
+      // Enquanto receber algo pela serial
+      while(mySerial.available() > 0) {
+        // Lê byte da serial
+        caractere = mySerial.read();
+        Serial.print("Recebi BT: ");
+        Serial.println(caractere);
+        // Concatena valores
+        bufferBT[i] = caractere;
+        i++;
+        bufferBT[i]= '\0';
+        // Aguarda buffer serial ler próximo caractere
+        vTaskDelay(1);
+      }
+      i=0;
+      if(bufferPC[0] != '\0'){
+        mySerial.println(bufferPC);
+        Serial.println(bufferPC);
+      }
+      bufferPC[0] = '\0'; 
+      xSemaphoreGive( xSerialSemaphore );
+
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
 
@@ -110,8 +202,6 @@ void TaskDigitalRead(void *pvParameters)  //Task para ler o botao.
 void TaskSemaforoPrincipal(void *pvParameters)  // Task blink para teste.
 {
   (void) pvParameters;
-  // Define o tempo em segundos para os estados do semaforo
-  int t_vermelho = 10, t_amarelo = 5, t_verde = 10;
   // inicializa os pinos do leds que representam os estados.
   pinMode(verde, OUTPUT);
   pinMode(amarelo, OUTPUT);
@@ -121,6 +211,7 @@ void TaskSemaforoPrincipal(void *pvParameters)  // Task blink para teste.
 
   for (;;) // A Task shall never return or exit.
   {
+
     digitalWrite(vermelho, LOW); 
     digitalWrite(verde, HIGH);   // liga o led verde para carro
     digitalWrite(p_vermelho, HIGH); //liga o led vermelho para pedestres
@@ -134,5 +225,7 @@ void TaskSemaforoPrincipal(void *pvParameters)  // Task blink para teste.
     digitalWrite(p_verde, HIGH);
     digitalWrite(vermelho, HIGH);  
     vTaskDelay( t_vermelho * 1000 / portTICK_PERIOD_MS ); // Tempo do estado vermelho carro
+    // Desliga flag
+    t_vermelho = 1;
   }
 }
